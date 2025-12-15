1b7004869268acfeae92f210e41d69129c00cbc3
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-12-15 18:42:12 -0500

    add new style

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 
 function Products() {
   return (
-    <div>
+    <div className='h-ful min-h-screen bg-gray-1000 dark:b-gray-900 p-8'>
       <header className="flex flex-row justify-between items-center bg-white dark:bg-gray-800 shadow-md p-6 rounded-lg mb-8 border border-gray-200 dark:border-gray-700">
         <h1 className="text-3xl font-bold text-gray-800 dark:text-white">Productos</h1>
         <Navbar />
--- src/pages/Sells.jsx
+++ src/pages/Sells.jsx
@@ -3,7 +3,7 @@ import SellForm from "./SellForm";
 
 function Sell() {
   return (
-    <div>
+    <div className='h-ful min-h-screen bg-gray-1000 dark:b-gray-900 p-8'>
       <header className="flex flex-row justify-between items-center bg-white dark:bg-gray-800 shadow-md p-6 rounded-lg mb-8 border border-gray-200 dark:border-gray-700">
         <h1 className="text-3xl font-bold text-gray-800 dark:text-white">Ventas</h1>
         <Navbar />
