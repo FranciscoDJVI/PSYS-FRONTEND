46124983e9798b9a91569caaa10f33af5e1e17fd
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-12-15 18:57:18 -0500

    delete status function and add new colors

--- src/components/ListSell.jsx
+++ src/components/ListSell.jsx
@@ -3,22 +3,6 @@ import { GetSell } from "../api/api.sell";
 import FormatterPesos from "../utils/CurrencyFormatter";
 
 
-const getCardColorClasses = (status) => {
-  const statusLower = status?.toLowerCase();
-
-  switch (statusLower) {
-    case 'confirmed':
-      return "border-l-4 border-green-500 bg-green-50 dark:bg-green-900/20";
-    case 'pending':
-      return "border-l-4 border-blue-500 bg-blue-50 dark:bg-blue-900/20";
-    case 'canceled':
-      return "border-l-4 border-red-500 bg-red-50 dark:bg-red-900/20";
-    default:
-      return "border-l-4 border-gray-400 bg-gray-50 dark:bg-gray-700/20";
-  }
-};
-
-
 function ListSellRegister() {
   const [sellRegister, setSellRegister] = useState([]);
 
@@ -28,7 +12,6 @@ function ListSellRegister() {
       try {
         const response = await GetSell();
         setSellRegister(response.data.results);
-        console.log(response.data);
       } catch (error) {
         console.error("Error al cargar ventas:", error);
       }
@@ -41,7 +24,7 @@ function ListSellRegister() {
       {sellRegister.map((sell) => (
         <div
           key={sell.sell_id}
-          className={`bg-white dark:bg-gray-800 p-6 rounded-2xl shadow-lg hover:shadow-2xl transition-all duration-300 border border-gray-200 dark:border-gray-700 hover:border-gray-300 dark:hover:border-gray-600 ${getCardColorClasses(sell.status)}`}
+          className={`bg-white dark:bg-gray-800 p-6 rounded-2xl shadow-lg hover:shadow-2xl transition-all duration-300 border border-gray-200 dark:border-gray-700 hover:border-gray-300 dark:hover:border-gray-600 ${(sell.status)}`}
         >
           <div className="mb-4">
             <p className="text-sm text-gray-500 dark:text-gray-400 uppercase tracking-wide">Fecha</p>
@@ -68,9 +51,9 @@ function ListSellRegister() {
           {Array.isArray(sell.sells) && sell.sells.map((sells, index) => (
             <div
               key={sells.id || index}
-              className='mb-3 p-4 rounded-lg transition-all duration-300 hover:scale-105'
+              className='p-2 rounded-lg transition-all duration-300 hover:scale-105'
             >
-              <div className="space-y-2 shadow-2xl bg-gray-900 p-4 rounded-2xl">
+              <div className="shadow-xl bg-gray-200  dark:bg-gray-900 p-4">
                 <p className="font-semibold text-gray-800 dark:text-gray-200">Producto: {sells.product_name}</p>
                 <p className="text-gray-700 dark:text-gray-300">Precio: <span className="font-bold text-black dark:text-white">{FormatterPesos(sells.product_price)}</span></p>
                 <p className="text-gray-700 dark:text-gray-300">Cantidad: <span className="font-bold text-gray-800 dark:text-gray-200">{sells.quantity}</span></p>
